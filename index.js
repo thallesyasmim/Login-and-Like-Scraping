40679f24fc2e8e45e070ec98941bf0ad78fd9608
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-21 14:52:35 -0300

    scroll drown and like

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const puppeteer = require('puppeteer')
         const email_input = await page.waitForXPath('//input[@name="username"]', { visible: true, timeout: 5000  })
         const password_input = await page.waitForXPath('//input[@type="password"]', { visible: true, timeout: 5000  })
         
-        await page.type('[name="username"]', process.env.EMAIL, { delay: 200 })
+        await page.type(email_input, process.env.EMAIL, { delay: 200 })
 
         await page.waitForTimeout(2000)
 
-        await page.type('[name="password"]', process.env.PASSWORD, { delay: 200 })
+        await page.type(password_input, process.env.PASSWORD, { delay: 200 })
       
         await page.click('button[type="submit"]', { button: 'left', delay: 500 })
 
@@ -25,7 +25,12 @@ const puppeteer = require('puppeteer')
 
         await page.waitForTimeout(5000)
 
-        await autoScroll(page, 100)
+        await autoScroll(page)
+
+        const button_like = await page.waitForXPath('//*[@id="react-root"]/section/main/section/div/div[2]/div/article[1]/div[3]/section[1]/span[1]/button[1]', { visible: true, timeout: 5000 })
+        await button_like.click()
+
+        
 
         //   await browser.close()
     } catch (error) {
@@ -35,20 +40,20 @@ const puppeteer = require('puppeteer')
 })()
 
 
-async function autoScroll(page, distance){
-    await page.evaluate(async () => {
-        await new Promise((resolve, reject) => {
+async function autoScroll(page){
+    return await page.evaluate(() => {
+        return new Promise((resolve, reject) => {
             var totalHeight = 0;
+            var distance = 100;
             var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
                 window.scrollBy(0, distance);
                 totalHeight += distance;
 
-                if(totalHeight >= scrollHeight){
+                if(totalHeight >= 700){
                     clearInterval(timer);
                     resolve();
                 }
-            }, 100);
+            }, 120);
         });
     });
 }
