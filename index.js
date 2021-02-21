c3351d5c09ad7b13bc0873f80a10de16ecffcc29
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-21 12:59:24 -0300

    script bash

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const puppeteer = require('puppeteer')
 ;(async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto('https://example.com')
-  await page.screenshot({ path: 'example.png' })
+  await page.goto('https://instagram.com')
 
-  await browser.close()
+//   await browser.close()
 })()
