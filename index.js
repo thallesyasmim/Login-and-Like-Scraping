8c1b430f5a1db3689a0ed0c64a642059f4a256bc
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-21 13:00:41 -0300

    headless false

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const puppeteer = require('puppeteer')
 
 ;(async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
   await page.goto('https://instagram.com')
 
